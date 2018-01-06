a1de020359e6bc2323c11e357bceefde513802b4
Matthew Berryhill <matthewberryhill@gmail.com>
2018-01-05 19:22:48 -0700

    Refactor setup sh to sudo properly

--- mine-setup.sh
+++ mine-setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo nvidia-smi -pl 105
+echo mcb1234 | sudo -S nvidia-smi -pl 105
 nvidia-settings -c :0 -a 'GPUFanControlState=1'
 nvidia-settings -c :0 -a 'GPUTargetFanSpeed=50'
 # nvidia-settings -c :0 -a 'GPUGraphicsClockOffset[3]==100'
